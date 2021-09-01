c967e6f5b6e6bb70810977fd22c3bf8703b6639b
EduardvonBriesen <e.v.briesen@googlemail.com>
2021-09-01 10:00:26 +0200

    Moved key values into variables #12

--- src/json_builder.rs
+++ src/json_builder.rs
@@ -1,54 +1,59 @@
+use serde::{Deserialize, Serialize};
 use serde_json::json;
 use serde_json::{Result, Value};
 
 pub fn get_json() -> Result<serde_json::Value> {
-    let cntr_pod = serde_json::from_value(json!({
-        "apiVersion": "v1",
-        "kind": "Pod",
-        "metadata": {
-          "name": "cntr"
-        },
-        "spec": {
-          "hostPID": true,
-          "containers": [
-            {
-              "name": "cntr",
-              "image": "onestone070/cntr",
-              "imagePullPolicy": "Always",
-              "command": [
-                "sleep",
-                "3600"
-              ],
-              "securityContext": {
-                "privileged": true,
-                "runAsUser": 0
-              },
-              "volumeMounts": [
-                {
-                  "name": "containerd-sock",
-                  "mountPath": "/run/containerd/containerd.sock"
-                }
-              ],
-              "env": [
-                {
-                  "name": "CONTAINERD_NAMESPACE",
-                  "value": "k8s.io"
-                }
-              ]
-            }
-          ],
-          "volumes": [
-            {
-              "name": "containerd-sock",
-              "hostPath": {
-                "path": "/run/k3s/containerd/containerd.sock",
-                "type": "Socket"
+  let image = "onestone070/cntr";
+  let mount_path = "/run/containerd/containerd.sock";
+  let path = "/run/k3s/containerd/containerd.sock";
+
+  let cntr_pod = serde_json::from_value(json!({
+      "apiVersion": "v1",
+      "kind": "Pod",
+      "metadata": {
+        "name": "cntr"
+      },
+      "spec": {
+        "hostPID": true,
+        "containers": [
+          {
+            "name": "cntr",
+            "image": image,
+            "imagePullPolicy": "Always",
+            "command": [
+              "sleep",
+              "3600"
+            ],
+            "securityContext": {
+              "privileged": true,
+              "runAsUser": 0
+            },
+            "volumeMounts": [
+              {
+                "name": "container-sock",
+                "mountPath": mount_path,
               }
+            ],
+            "env": [
+              {
+                "name": "CONTAINERD_NAMESPACE",
+                "value": "k8s.io"
+              }
+            ]
+          }
+        ],
+        "volumes": [
+          {
+            "name": "container-sock",
+            "hostPath": {
+              "path": path,
+              "type": "Socket"
             }
-          ],
-          "restartPolicy": "Never"
-        }
-    }))?;
-    
-    Ok(cntr_pod)
+          }
+        ],
+        "restartPolicy": "Never"
+      }
+  }))?;
+
+  Ok(cntr_pod)
 }
